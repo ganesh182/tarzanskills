3a151c98f71bd61eb0c30c0e15749d5c79a9e943
ganesh182 <ganeshhamsbavi@gmail.com>
2019-09-06 10:55:03 +0530

    operators

---
+++ day3.py
@@ -0,0 +1,9 @@
+first_name="abcac"
+no= 12
+length= len(first_name)
+remainder=no%length
+quo=no//length
+print(first_name*quo+first_name[-1])
+print(first_name*quo+first_name[1:10])
+print(first_name*quo+first_name[1:4])
+print(first_name[::-1])
