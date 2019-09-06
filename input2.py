ca0db1ef1909585fa052e0171f68b5db4951699c
ganesh182 <ganeshhamsbavi@gmail.com>
2019-09-06 14:09:16 +0530

    class result program

---
+++ input2.py
@@ -0,0 +1,13 @@
+name = input("enter the name")
+age = int(input("enter age"))
+clas = int(input("enter the clas"))
+phy = int(input("enter the marks in physics"))
+che = int(input("chemistry marks"))
+bio = int(input("biologyb marks"))
+ss = int(input("social science marks"))
+eng = int(input("english marks"))
+hin = int(input("hindi marrks"))
+sum = phy+bio+che+ss+eng+hin
+per = (sum/600)
+print("hello " + name)
+print("you have scored" , per )
