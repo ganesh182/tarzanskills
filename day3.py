a0fc9051feccd71be500e90253e726449ab2440f
ganesh182 <ganeshhamsbavi@gmail.com>
2019-09-06 11:51:01 +0530

    string

--- day3.py
+++ day3.py
@@ -1,9 +1,22 @@
-first_name="abcac"
+first_name="python"
 no= 12
 length= len(first_name)
 remainder=no%length
 quo=no//length
-print(first_name*quo+first_name[-1])
-print(first_name*quo+first_name[1:10])
-print(first_name*quo+first_name[1:4])
-print(first_name[::-1])
+#print(first_name*quo+first_name[-1])
+#print(first_name*quo+first_name[1:10])
+#print(first_name*quo+first_name[1:4])
+#print(first_name[::-1])
+
+print(first_name)
+print(first_name[0:5:1])
+print(first_name[0:5:2])
+print(first_name[0:5:3])
+print(first_name[0:5:4])
+print(first_name[0::])
+#print(first_name[6:0:-1])
+
+
+#print(first_name[5:3:-2])
+#print(first_name[6:2:-4])
+#print(first_name[3:0:-2])
